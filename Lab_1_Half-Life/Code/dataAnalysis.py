c255530dc4b97d73b9081a69b6af0b2063d3ab50
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-09-10 18:17:23 -0500

    merge

--- Lab_1_Half-Life/Code/dataAnalysis.py
+++ Lab_1_Half-Life/Code/dataAnalysis.py
@@ -2,50 +2,45 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+def sanniFunction():
+    def makeDataframe(filename) :
+        return pd.read_excel(f"Lab_1_Half-Life/Data/Formatted/{filename}", header=None)
 
-def makeDataframe(filename) :
-    return pd.read_excel(f"Lab_1_Half-Life/Data/Formatted/{filename}", header=None)
+    def logRegression(trial) :
+        indices = []
+        for i in range(len(trial[1])):
+            if trial[1][i] > 0:
+                indices.append(i)
+        time = []
+        logs = []
+        for i in indices :
+            time.append(trial[0][i])
+            logs.append(np.log(trial[1][i]))
+        return np.polyfit(time, logs, 1)
+        # return np.polyfit(trial[0], np.log(trial[1], where= trial[1] > 0), 1)
 
-def logRegression(trial) :
-    indices = []
-    for i in range(len(trial[1])):
-        if trial[1][i] > 0:
-            indices.append(i)
-    time = []
-    logs = []
-    for i in indices :
-        time.append(trial[0][i])
-        logs.append(np.log(trial[1][i]))
-    return np.polyfit(time, logs, 1)
-    # return np.polyfit(trial[0], np.log(trial[1], where= trial[1] > 0), 1)
+    def logPlot(trial) :
+        a,b = logRegression(trial)
+        scatterPlot = plt.scatter(trial[0], np.log(trial[1]), color='teal')
+        regressionPlot = plt.plot(trial[0], a*trial[0]+b, color='red')
+        plt.show()
+        return a,b
 
-def logPlot(trial) :
-    a,b = logRegression(trial)
-    scatterPlot = plt.scatter(trial[0], np.log(trial[1]), color='teal')
-    regressionPlot = plt.plot(trial[0], a*trial[0]+b, color='red')
-    plt.show()
-    return a,b
+    trial1 = makeDataframe("LiquidTrial_1_Excel.xlsx")
+    trial2 = makeDataframe("LiquidTrial_2_Excel.xlsx")
+    trial3 = makeDataframe("LiquidTrial_3_Excel.xlsx")
+    trial4 = makeDataframe("LiquidTrial_4_Excel.xlsx")
 
-trial1 = makeDataframe("LiquidTrial_1_Excel.xlsx")
-trial2 = makeDataframe("LiquidTrial_2_Excel.xlsx")
-trial3 = makeDataframe("LiquidTrial_3_Excel.xlsx")
-trial4 = makeDataframe("LiquidTrial_4_Excel.xlsx")
+    plots1 = logPlot(trial1)
+    plots2 = logPlot(trial2)
+    plots3 = logPlot(trial3)
+    plots4 = logPlot(trial4)
 
-plots1 = logPlot(trial1)
-plots2 = logPlot(trial2)
-plots3 = logPlot(trial3)
-plots4 = logPlot(trial4)
-
-print(np.log(2)/(-plots1[0]))
-print(np.log(2)/(-plots2[0]))
-print(np.log(2)/(-plots3[0]))
-print(np.log(2)/(-plots4[0]))
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-from scipy.optimize import root
-
+    print(np.log(2)/(-plots1[0]))
+    print(np.log(2)/(-plots2[0]))
+    print(np.log(2)/(-plots3[0]))
+    print(np.log(2)/(-plots4[0]))
+# sanniFunction()
 def analyzeData(filename):
 
     def neg_ln_func(x, a, b):
@@ -66,7 +61,7 @@ def analyzeData(filename):
     print("The half life is approximately", np.exp((.5 * b - b)/(-1 * a)) )
     plt.show()
     
-(analyzeData("LiquidTrial_1_Excel.xlsx"))
+# (analyzeData("LiquidTrial_1_Excel.xlsx"))
 # (analyzeData("LiquidTrial_2_Excel.xlsx"))
 # (analyzeData("LiquidTrial_3_Excel.xlsx"))
 # (analyzeData("Li
