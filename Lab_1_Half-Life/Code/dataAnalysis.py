085e4869f16a5a659c4ac8c279ee072e546d702f
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-09-11 16:11:00 -0500

    lab data and analysis from day 3

--- Lab_1_Half-Life/Code/dataAnalysis.py
+++ Lab_1_Half-Life/Code/dataAnalysis.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.optimize import root
+import math
+from pprint import pprint
 def sanniFunction():
     def makeDataframe(filename) :
         return pd.read_excel(f"Lab_1_Half-Life/Data/Formatted/{filename}", header=None)
@@ -26,42 +29,62 @@ def sanniFunction():
         plt.show()
         return a,b
 
-    trial1 = makeDataframe("LiquidTrial_1_Excel.xlsx")
-    trial2 = makeDataframe("LiquidTrial_2_Excel.xlsx")
-    trial3 = makeDataframe("LiquidTrial_3_Excel.xlsx")
-    trial4 = makeDataframe("LiquidTrial_4_Excel.xlsx")
+    # trial1 = makeDataframe("LiquidTrial_1_Excel.xlsx")
+    # trial2 = makeDataframe("LiquidTrial_2_Excel.xlsx")
+    # trial3 = makeDataframe("LiquidTrial_3_Excel.xlsx")
+    # trial4 = makeDataframe("LiquidTrial_4_Excel.xlsx")
 
-    plots1 = logPlot(trial1)
-    plots2 = logPlot(trial2)
-    plots3 = logPlot(trial3)
-    plots4 = logPlot(trial4)
+    # plots1 = logPlot(trial1)
+    # plots2 = logPlot(trial2)
+    # plots3 = logPlot(trial3)
+    # plots4 = logPlot(trial4)
 
-    print(np.log(2)/(-plots1[0]))
-    print(np.log(2)/(-plots2[0]))
-    print(np.log(2)/(-plots3[0]))
-    print(np.log(2)/(-plots4[0]))
+    # print(np.log(2)/(-plots1[0]))
+    # print(np.log(2)/(-plots2[0]))
+    # print(np.log(2)/(-plots3[0]))
+    # print(np.log(2)/(-plots4[0]))
+    
+    # trial5 = makeDataframe("day3_liquidtrial1.xlsx")
+    # a,b = logPlot(trial5)
+    # print(np.log(2)/(-a)/3)
 # sanniFunction()
-def analyzeData(filename):
 
-    def neg_ln_func(x, a, b):
-        return -a * np.log(x) + b
+def analyzeData(filename):
+    def getBackgroundAverage(filename):
+        data = pd.read_excel("Lab_1_Half-Life/Data/Formatted/" + filename, header=None)
+        x_axis = np.array(data[0])
+        y_axis = np.array(data[1])
+        avg = np.average(y_axis)
+        return avg
+    def exp_func(x, a, b, c):
+        return a * math.e**(-1 * b * x) + c 
     data = pd.read_excel("Lab_1_Half-Life/Data/Formatted/" + filename, header=None)
     x_axis = np.array(data[0])
-    y_axis = np.array(data[1])
-    params, covariance = curve_fit(neg_ln_func, x_axis, y_axis)
-    a, b = params
-    print(f"The equation of the line is: y = -{a:.2f} ln(x) + {b:.2f}")
-
+    backgroundAverage = getBackgroundAverage("backgroundradiation.xlsx")
+    y_axis = np.array(data[1]) - backgroundAverage
+    params, covariance = curve_fit(exp_func, x_axis, y_axis)
+    params = tuple(params)
+    a, b, c = params
+    perr = np.sqrt(np.diag(covariance))
+    berr = perr[1]
+    # print(f'bg = {backgroundAverage}')
+    print(f"The equation of the line is: y = -{a:.2f} exp(-{b:.5f}*x) + {c}")
+    print(f"The decay constant is {b:.5f} \u00B1 {berr:.5f}")
     plt.scatter(x_axis, y_axis, label='Data')
-    plt.plot(x_axis, neg_ln_func(x_axis, *params), label='Fitted Curve', color='red')
+    plt.plot(x_axis, exp_func(x_axis, *params), label='Fitted Curve', color='red')
     plt.legend()
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.title('Logarithmic Curve Fit')
-    print("The half life is approximately", np.exp((.5 * b - b)/(-1 * a)) )
-    plt.show()
+    plt.title('Exponential Curve Fit')
+    halfLife = np.log(2)/b
+    halfLifeErr = np.log(2)/np.power(b,2)*berr
+    print(f"Half life is {halfLife:.1f} \u00B1 {halfLifeErr:.1f}")
+    # plt.show()
     
-# (analyzeData("LiquidTrial_1_Excel.xlsx"))
-# (analyzeData("LiquidTrial_2_Excel.xlsx"))
-# (analyzeData("LiquidTrial_3_Excel.xlsx"))
-# (analyzeData("Li
+# (analyzeData("day3_liquidtrial1.xlsx"))
+# (analyzeData("day3_liquidtrial2.xlsx"))
+# (analyzeData("day3_liquidtrial3.xlsx"))
+(analyzeData("day3_liquidtrial4.xlsx"))
+
+
+    
