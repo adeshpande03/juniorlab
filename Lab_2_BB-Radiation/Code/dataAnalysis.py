50320624e4349e5e48d49b81a3fb20dfd6c280a2
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-09-27 16:58:38 -0500

    new code

--- Lab_2_BB-Radiation/Code/dataAnalysis.py
+++ Lab_2_BB-Radiation/Code/dataAnalysis.py
@@ -10,13 +10,33 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+# input: wavelength in meters
+def refractiveIndex(wv):
+    wv = wv * 1e6  # convert wavelength to microns
+    wvSquared = wv**2
+    n = np.sqrt(
+        1.33973
+        + 0.81070 * wvSquared / (wvSquared - 0.10065**2)
+        + 0.19652 * wvSquared / (wvSquared - 29.87**2)
+        + 4.52469 * wvSquared / (wvSquared - 53.82**2)
+    )
+    return n
+
+
+def angularDeflection(wv):
+    n = refractiveIndex(wv)
+    phi = np.deg2rad(47.447)
+    pi = np.pi
+    alpha = np.deg2rad(60)
+    d = phi + np.arcsin(n * np.sin(alpha - np.arcsin(np.sin(phi) / n))) - alpha
+    d = np.rad2deg(d)
+    return d
+
+
 def analyzeData(filename):
     def readRaw(filename):
         data = pd.read_table("Lab_2_BB-Radiation/Raws/" + filename, delimiter="\t")
         return data
-
-    def refractiveIndex(wv):
-        n = np.sqrt(1.33973 + 0.81070*wv**2/(wv**2 - 0.10065**2) + 0.19652 *wv**2/(wv**2 - 29.87**2) + 4.52469 * wv**2/(wv**2 - 53.82**2))
 
     data = readRaw(filename)
     x_axis = np.array(data["angle (degrees) - Plot 0"])
@@ -37,17 +57,23 @@ def analyzeData(filename):
         y_axis = y_axis[mask]
     plt.ylim(0, 0.009)
     plt.scatter(x_axis, y_axis, label=filename[3:-4])
+
+    wvRange = np.linspace(0.2e-6, 11e-6, 120)
+    deflections = np.vectorize(angularDeflection)(wvRange)
+
     plt.legend()
     plt.xlabel("Angle")
     plt.ylabel("Intensity")
     plt.title("Intensity vs. Angle")
-    plt.savefig(f"Lab_2_BB-Radiation/Images/{filename}_plot.png")
+    # plt.savefig(f"Lab_2_BB-Radiation/Images/{filename}_plot.png")
     plt.clf()
 
 
 if __name__ == "__main__":
-    for temp in [600, 650, 700, 800, 900, 1000, 1100]:
-        if temp == 700:
-            (analyzeData(f"d4_{temp}C.txt"))
-            continue
-        (analyzeData(f"d3_{temp}C.txt"))
+    # for temp in [600, 650, 700, 800, 900, 1000, 1100]:
+    #     if temp == 700:
+    #         (analyzeData(f"d4_{temp}C.txt"))
+    #         continue
+    #     (analyzeData(f"d3_{temp}C.txt"))
+    # print(angularDeflection(635e-9))
+    analyzeData(f"d3_600C.txt")
