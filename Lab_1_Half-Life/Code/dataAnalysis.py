9b26a4f59953f8fe96c81d375bcfb54032dc9f0d
danny2004de <desaisannidhya43@gmail.com>
2023-10-01 02:06:57 -0500

    graph spectral radiance and other laws

--- Lab_1_Half-Life/Code/dataAnalysis.py
+++ Lab_1_Half-Life/Code/dataAnalysis.py
@@ -66,6 +66,8 @@ def analyzeData(filename):
         data = readRaw(filename)
         y_axis = np.array(data[1])
         avg = np.average(y_axis)
+        std = np.std(y_axis)
+        # print(avg, std)
         return avg
 
     def exp_func(x, a, b, c):
@@ -79,24 +81,27 @@ def analyzeData(filename):
     params = tuple(params)
     a, b, c = params
     perr = np.sqrt(np.diag(covariance))
+    aerr = perr[0]
     berr = perr[1]
+    cerr= perr[2]
     # print(f'bg = {backgroundAverage}')
-    # print(f"The equation of the line is: y = -{a:.2f} exp(-{b:.5f}*x) + {c}")
+    # print(f"The equation of the line is: y = ({a:.3} +- {aerr:.3}) exp(-({b:.3} +- {berr:.3})*x) + {c:.3} +- {cerr:.3}")
     # print(f"The decay constant is {b:.5f} \u00B1 {berr:.5f}")
     plt.scatter(x_axis, y_axis, label="Data")
     plt.plot(x_axis, exp_func(x_axis, *params), label="Logarithmic Fit", color="red")
     plt.legend()
-    plt.xlabel("Time")
-    plt.ylabel("Gamma Ray Count")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Gamma Ray Counts (Bq)")
     plt.title("Barium-137 Gamma Ray Counts vs. Time")
-    plt.text(x_axis.max() / 3 * 2, y_axis.max() / 4 * 3,f'y = -{a:.2E} exp(-{b:.2E}•x) + {c:.2E}', horizontalalignment='center',
+    plt.text(x_axis.max() / 3 * 2, y_axis.max() / 4 * 3,f'y = {a:.2E} exp(-{b:.2E}•x) + {c:.2E}', horizontalalignment='center',
      verticalalignment='center', color='red')
     halfLife = np.log(2) / b
     halfLifeErr = np.log(2) / np.power(b, 2) * berr
-    # print(f"Half life is {halfLife:.1f} \u00B1 {halfLifeErr:.1f}")
+    print(f"Half life is {halfLife:.1f} \u00B1 {halfLifeErr:.1f}")
     plt.savefig(f"Lab_1_Half-Life/Images/{filename}_plot.png")
     plt.clf()
-    return {"halfLife": halfLife, "error": halfLifeErr}
+    return {"decayconstant": b, "dcerror": berr, "halfLife": halfLife, "hferror": halfLifeErr}
+        
 
 
 if __name__ == "__main__":
@@ -106,3 +111,14 @@ if __name__ == "__main__":
     (analyzeData("day3_liquidtrial4"))
     (analyzeData("day3_liquidtrial5"))
     (analyzeData("day3_liquidtrial6"))
+    # filename = "backgroundradiation"
+    # data = pd.read_table("Lab_1_Half-Life/Data/Raws/" + filename, delimiter="\t", header=None)
+    # y_axis = np.array(data[1])
+    # x_axis = np.array(data[0])
+    # plt.scatter(x_axis, y_axis)
+    # plt.xlabel("Time (s)")
+    # plt.ylabel("Background Radiation Count (Bq)")
+    # plt.title("Background Radiation Counts vs. Time")
+    # plt.savefig(f"Lab_1_Half-Life/Images/{filename}_plot.png")
+    # plt.clf()
+    
