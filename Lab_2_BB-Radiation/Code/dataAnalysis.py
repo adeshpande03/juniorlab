9ba1ca901975c557174e7c9c50e4d20b1b1c8510
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-10-01 15:37:18 -0500

    minor changes

--- Lab_2_BB-Radiation/Code/dataAnalysis.py
+++ Lab_2_BB-Radiation/Code/dataAnalysis.py
@@ -37,15 +37,14 @@ def analyzeData(filename):
     def readRaw(filename):
         data = pd.read_table("Lab_2_BB-Radiation/Raws/" + filename, delimiter="\t")
         return data
-    
+
     def find_nearest(array, value):
         array = np.asarray(array)
         idx = (np.abs(array - value)).argmin()
         return idx
-    
+
     def planck_law(wv, a, b):
-        return a/(wv**5) * 1/(np.exp(b/wv)-1)
-        
+        return a / (wv**5) * 1 / (np.exp(b / wv) - 1)
 
     data = readRaw(filename)
     x_axis = np.array(data["angle (degrees) - Plot 0"])
@@ -57,7 +56,7 @@ def analyzeData(filename):
         # Filter both x and y using the mask
         x_axis = x_axis[mask]
         y_axis = y_axis[mask]
-        
+
     elif "700C" in filename:
         mask = (y_axis <= 0.005) & (y_axis >= -0.001)
 
@@ -74,11 +73,11 @@ def analyzeData(filename):
     # plt.ylim(0, 0.009)
     # plt.scatter(x_axis, y_axis, label=f"$T = ${filename[3:-5]}$\degree$C", s=3)
 
-    wvRange = np.linspace(0.2, 11, 1000) 
+    wvRange = np.linspace(0.2, 11, 1000)
     deflections = np.vectorize(angularDeflection)(wvRange)
     # deltafunction = interp1d(wvRange, deflections, kind="cubic")
     lambdafunction = interp1d(deflections, wvRange, kind="cubic")
-    grad = -1*np.gradient(deflections, wvRange)
+    grad = -1 * np.gradient(deflections, wvRange)
     # gradfunction = interp1d(wvRange, grad, kind="cubic")
     wavelengths = np.vectorize(lambdafunction)(x_axis)
     grads = []
@@ -87,7 +86,7 @@ def analyzeData(filename):
     grads = np.asarray(grads)
     bofWV = np.multiply(y_axis, grads)
     # params, covariance = curve_fit(planck_law, np.delete(wavelengths, [80, 81, 91, 92]), np.delete(bofWV, [80, 81, 91, 92]), p0=[10,10])
-    params, covariance = curve_fit(planck_law,wavelengths, bofWV, p0=[10,10])
+    params, covariance = curve_fit(planck_law, wavelengths, bofWV, p0=[10, 10])
     params = tuple(params)
     a, b = params
     perr = np.sqrt(np.diag(covariance))
@@ -97,14 +96,14 @@ def analyzeData(filename):
     pl = planck_law(wvRange, *params)
     wvMax = wvRange[np.argmax(pl)]
     integral = simpson(pl, wvRange)
-    
+
     plt.scatter(wavelengths, bofWV, label=f"$T = ${filename[3:-5]}$\degree$C", s=2)
     plt.plot(wvRange, planck_law(wvRange, *params))
     # plt.plot(wavelengths, planck_law(wavelengths, *(params+perr)), color="green", linestyle='dashed')
     # plt.plot(wavelengths, planck_law(wavelengths, *(params-perr)), color="green", linestyle='dashed')
-    
+
     # plt.xlim(left = 1)
-    # plt.ylim(bottom = -0.0001)
+    # plt.ylim(top = 0.0025)
     plt.legend()
     # plt.xlabel("$\delta$   (degrees)")
     plt.xlabel("$\lambda$ ($\mu$m)")
@@ -113,17 +112,22 @@ def analyzeData(filename):
     # plt.title("Relative Spectral Radiance vs. Deflection Angle")
     plt.title("Relative Spectral Radiance vs. Wavelength")
     # plt.show()
-    # plt.savefig(f"Lab_2_BB-Radiation/Images/{filename}_angleplot.png")
+    plt.savefig(f"Lab_2_BB-Radiation/Images/{filename}_angleplot.png")
     plt.clf()
-    error = 1e-6*np.sqrt(b**2 * 0.1**2 + berr**2 * (int(filename[3:-5])+273.15)**2)
+    error = 1e-6 * np.sqrt(
+        b**2 * 0.1**2 + berr**2 * (int(filename[3:-5]) + 273.15) ** 2
+    )
     # return (b*(int(filename[3:-5])+273.15)*1e-6, berr*(int(filename[3:-5])+273.15)*1e-6, wvMax, integral)
-    return (b*(int(filename[3:-5])+273.15)*1e-6, error, wvMax, integral)
+    return (b * (int(filename[3:-5]) + 273.15) * 1e-6, error, wvMax, integral)
+
 
 def wien_law(temp, a):
-    return a/temp
+    return a / temp
+
 
 def stefanboltzmann_law(temp, a):
     return temp**4 * a
+
 
 if __name__ == "__main__":
     temps = [600, 650, 700, 800, 900, 1000, 1100]
@@ -133,8 +137,9 @@ if __name__ == "__main__":
     integrals = []
     for temp in temps:
         if temp == 700:
-            output = (analyzeData(f"d4_{temp}C.txt"))
-        else: output = (analyzeData(f"d3_{temp}C.txt"))
+            output = analyzeData(f"d4_{temp}C.txt")
+        else:
+            output = analyzeData(f"d3_{temp}C.txt")
         bs.append(output[0])
         berrs.append(output[1])
         wvMaxes.append(output[2])
@@ -142,24 +147,24 @@ if __name__ == "__main__":
         # print(output)
     # print(angularDeflection(635e-9))
     # analyzeData(f"d3_600C.txt")
-    
+
     # get average hc/k
     b = np.mean(bs)
     berr = np.linalg.norm(berrs)
     print(b, berr)
-    
+
     # graph lambda_max vs temperature
     temps = np.asarray(temps)
-    params, covariance = curve_fit(wien_law, temps+273.15, wvMaxes)
+    params, covariance = curve_fit(wien_law, temps + 273.15, wvMaxes)
     a = params[0]
     perr = np.sqrt(np.diag(covariance))
     aerr = perr[0]
-    plt.scatter(1/(temps+273.15), wvMaxes)
-    plt.plot(1/(temps+273.15), wien_law(temps+273.15, a), color="red")
+    plt.scatter(1 / (temps + 273.15), wvMaxes)
+    plt.plot(1 / (temps + 273.15), wien_law(temps + 273.15, a), color="red")
     plt.xlabel("$1/T$ (K$^{-1}$)")
     plt.ylabel("$\lambda_{max}$ ($\mu$m)")
     # plt.show()
-    # plt.savefig(f"Lab_2_BB-Radiation/Images/wienslawplot.png")
+    plt.savefig(f"Lab_2_BB-Radiation/Images/wienslawplot.png")
     plt.clf()
     print(a, aerr)
     # residuals = wvMaxes - wien_law(temps+273.15, a)
@@ -167,22 +172,21 @@ if __name__ == "__main__":
     # ss_tot = np.sum((wvMaxes-np.mean(wvMaxes))**2)
     # r_squared = 1- ss_res/ss_tot
     # print(r_squared)
-    
+
     # graph total power vs temperature]
-    params, covariance = curve_fit(stefanboltzmann_law, temps+273.15, integrals)
+    params, covariance = curve_fit(stefanboltzmann_law, temps + 273.15, integrals)
     a = params[0]
     perr = np.sqrt(np.diag(covariance))
     aerr = perr[0]
-    plt.scatter((temps+273.15)**4, integrals)
-    plt.plot((temps+273.15)**4, stefanboltzmann_law(temps+273.15, a), color="red")
+    plt.scatter((temps + 273.15) ** 4, integrals)
+    plt.plot((temps + 273.15) ** 4, stefanboltzmann_law(temps + 273.15, a), color="red")
     plt.xlabel("$T^4$ (K$^4$)")
     plt.ylabel("$\int B(\lambda,T) d\lambda$")
     # plt.show()
-    # plt.savefig(f"Lab_2_BB-Radiation/Images/stefanboltzmannplot.png")
-    print(a,aerr)
+    plt.savefig(f"Lab_2_BB-Radiation/Images/stefanboltzmannplot.png")
+    print(a, aerr)
     # residuals = integrals - stefanboltzmann_law(temps+273.15, a)
     # ss_res = np.sum(residuals**2)
     # ss_tot = np.sum((integrals-np.mean(integrals))**2)
     # r_squared = 1- ss_res/ss_tot
     # print(r_squared)
-    
