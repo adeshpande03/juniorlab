9d4c9bdedbf96b2a3ced17a4d1248498ab0e96ed
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-10-11 15:00:19 -0500

    new puhs

--- Lab_3_Brown/Code/dataAnalysis.py
+++ Lab_3_Brown/Code/dataAnalysis.py
@@ -4,6 +4,7 @@ from skimage import io
 from scipy.optimize import linear_sum_assignment
 from pprint import *
 
+
 def detect_dots(image):
     # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = image
@@ -18,26 +19,31 @@ def detect_dots(image):
 
 def compute_displacement(dots1, dots2, frame_num):
     results = []
-    
+
     if len(dots1) == 0 or len(dots2) == 0:
         return results
-    
+
     # Compute pairwise distance matrix
-    distance_matrix = np.linalg.norm(dots1[:, np.newaxis, :2] - dots2[np.newaxis, :, :2], axis=2)
-    
+    distance_matrix = np.linalg.norm(
+        dots1[:, np.newaxis, :2] - dots2[np.newaxis, :, :2], axis=2
+    )
+
     # Use Hungarian algorithm to find optimal assignment
     row_ind, col_ind = linear_sum_assignment(distance_matrix)
 
     for r, c in zip(row_ind, col_ind):
         displacement = distance_matrix[r, c]
-        results.append({
-            'dot_id': r,
-            'frame': frame_num,
-            'position': tuple(dots2[c][:2]),
-            'displacement': displacement
-        })
-    
+        results.append(
+            {
+                "dot_id": r,
+                "frame": frame_num,
+                "position": tuple(dots2[c][:2]),
+                "displacement": displacement,
+            }
+        )
+
     return results
+
 
 def track_dot_displacement(tif_file):
     images = io.imread(tif_file)
@@ -54,13 +60,13 @@ def track_dot_displacement(tif_file):
     return all_results
 
 
-
 tif_file = "Lab_3_Brown/Data/Day 1 Trial 1 - 1 micron - 10x magnification/image_0.tif"
 displacements = track_dot_displacement(tif_file)
 # print(displacements)
 # np.save(f"{tif_file[:-4]}.txt", displacements)
 with open(f"{tif_file[:-4]}.txt", "w") as f:
+    f.write("[")
     for line in displacements:
-        f.write(f"{line}\n")
-
+        f.write(f"{line},\n")
+    f.write("]")
 pprint(displacements)
